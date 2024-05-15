f148a650a65a67f7a2fec3436da6624aeaf117ee
Juvenaawh <144326106+Juvenaawh@users.noreply.github.com>
2024-05-15 22:54:56 +0200

    added sendrequest method in OrderController

--- src/main/java/carport/controllers/OrderController.java
+++ src/main/java/carport/controllers/OrderController.java
@@ -1,6 +1,10 @@
 package carport.controllers;
 
+import carport.entities.User;
+import carport.exceptions.DatabaseException;
 import carport.persistence.ConnectionPool;
+import carport.persistence.ProductMapper;
+import carport.persistence.UserMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,15 +13,10 @@ public class OrderController {
     }
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("/order", ctx -> {
-            ctx.render("showorder.html");
-        });
-        app.post("/save-dimensions", ctx -> {
-            saveDimensions(ctx);
-        });
-        app.get("/calculate", ctx -> {
-            calculatePrice(ctx);
-        });
+        app.get("/order", ctx -> ctx.render("showorder.html"));
+        app.post("/save-dimensions", ctx -> saveDimensions(ctx));
+        app.get("/sendrequest", ctx -> sendRequest(ctx, connectionPool));
+        app.get("/calculate", ctx -> calculatePrice(ctx));
     }
 
     private static void saveDimensions(Context ctx) {
@@ -34,6 +33,29 @@ public class OrderController {
         } catch (NumberFormatException e) {
             ctx.attribute("error", "Ugyldigt. Vælg venligst både bredde og længde.");
             ctx.render("byg-selv.html");
+        }
+    }
+
+    private static void sendRequest(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+
+        int width = ctx.sessionAttribute("width");
+        int length = ctx.sessionAttribute("length");
+
+        try{
+    // TODO: Der skal vel et user objekt eller user_id som argument i InsertCarportCustomBase ellers kan kunden ikke se sin ordre?
+    // TODO: Eller er det først når admin/sælger har godkendt forespørgslen at den gemmes på user_id og kunden kan se ordren?
+
+            /*User user = UserMapper.login(email, password, cp);
+            ctx.sessionAttribute("currentUser", user);*/
+
+            ProductMapper.InsertCarportCustomBase(connectionPool, length, width, 200);
+
+            // TODO: Giv kunde besked om at forespørgslen er sendt
+
+        }catch (DatabaseException e){
+            ctx.attribute("message", "Fejl ved afsendelse af forespørgsel. Prøv igen. " +
+                    "Hvis fejlen fortsætter, så kontakt en sælger.");
+            ctx.render("svg.html");
         }
     }
 
